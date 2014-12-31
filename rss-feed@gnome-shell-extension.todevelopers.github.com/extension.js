6ed7ac63e624cd039f1312564f24823de2947b82
Tomáš Gažovič <gazovic.tomas@gmail.com>
2014-12-31 14:32:04 +0100

    test basic layout

--- rss-feed@gnome-shell-extension.todevelopers.github.com/extension.js
+++ rss-feed@gnome-shell-extension.todevelopers.github.com/extension.js
@@ -18,26 +18,55 @@ const RssFeedButton = new Lang.Class({
     _init: function() {
         this.parent(0.0, "RSS Feed");
 
+        /* top panel button */
         let icon = new St.Icon({
             icon_name: 'application-rss+xml-symbolic',
             style_class: 'system-status-icon'
         });
 
         this.actor.add_actor(icon);
+
+        /* menu items*/
+        let testarea = new PopupMenu.PopupBaseMenuItem({
+            reactive: false
+        });
+
+        let testlabel = new St.Label({
+            text: 'Lorem ipsum dolor sit amet'
+        });
+
+        testarea.actor.add_actor(testlabel);
+        this.menu.addMenuItem(testarea);
+
+        let item = new PopupMenu.PopupSeparatorMenuItem();
+        this.menu.addMenuItem(item);
+
+        let buttonMenu = new PopupMenu.PopupBaseMenuItem({
+            reactive: false
+        });
+
+        let systemMenu = Main.panel.statusArea.aggregateMenu._system;
+
+        let refreshBtn = systemMenu._createActionButton('view-refresh-symbolic', "Refresh");
+        let settingsBtn = systemMenu._createActionButton('preferences-system-symbolic', "RSS Feed Settings");
+        buttonMenu.actor.add_actor(refreshBtn);
+        buttonMenu.actor.add_actor(settingsBtn);
+
+        this.menu.addMenuItem(buttonMenu);
     }
 });
 
-let rssFeedButton;
+let rssFeedBtn;
 
 function init() {
 
 }
 
 function enable() {
-    rssFeedButton = new RssFeedButton();
-    Main.panel.addToStatusArea('rssFeedMenu', rssFeedButton, 0, 'right');
+    rssFeedBtn = new RssFeedButton();
+    Main.panel.addToStatusArea('rssFeedMenu', rssFeedBtn, 0, 'right');
 }
 
 function disable() {
-    rssFeedButton.destroy();
+    rssFeedBtn.destroy();
 }
