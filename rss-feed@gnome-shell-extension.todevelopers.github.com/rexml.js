2b40ebd74fa781567f9cd1500807bcffa7beed4a
wxf <xiaofeng.wang@bqvision.com>
2016-03-04 15:29:33 +0800

    support new attribute lines after

--- rss-feed@gnome-shell-extension.todevelopers.github.com/rexml.js
+++ rss-feed@gnome-shell-extension.todevelopers.github.com/rexml.js
@@ -147,7 +147,7 @@ function REXML(XML) {
 
 function ParseAttribute(str,Attribute) {
 	var str = str +  ">";
-	if (str.indexOf(Attribute + "='")>-1) var Attr = new RegExp("(.*\\n)*.*" + Attribute + "='([^']*)'.*>");
-	else if (str.indexOf(Attribute + '="')>-1) var Attr = new RegExp("(.*\\n)*.*" + Attribute + '="([^"]*)".*>');
+	if (str.indexOf(Attribute + "='")>-1) var Attr = new RegExp("(.*\\n)*.*" + Attribute + "='([^']*)'(.*\\n)*.*>");
+	else if (str.indexOf(Attribute + '="')>-1) var Attr = new RegExp("(.*\\n)*.*" + Attribute + '="([^"]*)"(.*\\n).*>');
 	return str.replace(Attr, "$2");
 }
